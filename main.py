8c8991fb274f233add15e5d768f0d7d2f498a94b
Ishaan <bhardwaj.ishaan11@gmail.com>
2023-06-27 13:51:43 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+person = input("Enter a name: ")
+date = input("Enter a date (Month Day, Year): ")
+place = input("Enter a place: ")
+monster = input("Enter a monster: ")
+danger = input("Enter something people do when they are in danger: ")
+item = input("Name an item used to hurt someone: ")
+
+
+print(f'''There was once a person named {person}. On {date}, {person} went to {place}. There, {person} saw {monster}.  
+Everyone else started to {danger}. {person} was the only one left to defeat {monster}. Then, {person} picked up a {item} 
+and prepared to fight {monster}''')
